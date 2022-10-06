267ec0aacf37569da0e5a4ab1b7c2424eec956ea
Alina <alina.kirov@mail.ru>
2022-10-06 22:24:51 +0300

    скорректировала

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ let answer = 55;
 function check() {
     
     quastion = prompt("Угадай число от 1 до 100");
-
-    if (quastion > 100 || quastion < 0) {
+    
+    if (quastion === null) {
+        alert("Игра закончена");
+    } else if (quastion > 100 || quastion < 0) {
         alert("Число должно быть от 1 до 100!");
         check();  
+    } else if (quastion.length == 0 || isNaN(quastion) || (/^\s*$/.test(quastion))) {
+        alert("Введи число!");
+        check();
     } else if (quastion > answer) {
         alert("Загаданное число меньше");
         check();
     } else if (quastion < answer) {
         alert ("Загаданное число больше");
         check();
-    } else if (isNaN(quastion)) {
-        alert("Введи число!");
-        check();
-    } else if (quastion === null) {
-        alert("Игра закончена");
     } else if (quastion <= 100 && quastion >= 0) {
         return confirm("Вы угадали!");
     };

a626db144aa4848f9f6d59eb504ecc771eb40030
Alina <alina.kirov@mail.ru>
2022-10-06 22:01:19 +0300

    выполнено

---
+++ script.js
@@ -0,0 +1,27 @@
+let quastion;
+let answer = 55;
+
+
+function check() {
+    
+    quastion = prompt("Угадай число от 1 до 100");
+
+    if (quastion > 100 || quastion < 0) {
+        alert("Число должно быть от 1 до 100!");
+        check();  
+    } else if (quastion > answer) {
+        alert("Загаданное число меньше");
+        check();
+    } else if (quastion < answer) {
+        alert ("Загаданное число больше");
+        check();
+    } else if (isNaN(quastion)) {
+        alert("Введи число!");
+        check();
+    } else if (quastion === null) {
+        alert("Игра закончена");
+    } else if (quastion <= 100 && quastion >= 0) {
+        return confirm("Вы угадали!");
+    };
+}
+check();

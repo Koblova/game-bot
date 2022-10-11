c3a5794851f0d8c5e786adb518f4c8ffc7ec0ed1
Alina <alina.kirov@mail.ru>
2022-10-11 20:02:19 +0300

    правки

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let quastion;
 let answer;
 
-
-function check() {
-
-    answer = 55;
-    quastion = prompt("Угадай число от 1 до 100");
-    
-    if (quastion === null) {
-        alert("Игра закончена");
-    } else if (quastion > 100 || quastion < 0) {
-        alert("Число должно быть от 1 до 100!");
-        check();  
-    } else if (quastion.length == 0 || isNaN(quastion) || (/^\s*$/.test(quastion))) {
-        alert("Введи число!");
-        check();
-    } else if (quastion > answer) {
-        alert("Загаданное число меньше");
-        check();
-    } else if (quastion < answer) {
-        alert ("Загаданное число больше");
-        check();
-    } else if (quastion <= 100 && quastion >= 0) {
-        return confirm("Вы угадали!");
-    };
+function game(answer) {
+    function check() {
+        quastion = prompt("Угадай число от 1 до 100");
+        
+        if (quastion === null) {
+            alert("Игра закончена");
+        } else if (quastion > 100 || quastion < 0) {
+            alert("Число должно быть от 1 до 100!");
+            check();  
+        } else if (quastion.length == 0 || isNaN(quastion) || (/^\s*$/.test(quastion))) {
+            alert("Введи число!");
+            check();
+        } else if (quastion > answer) {
+            alert("Загаданное число меньше");
+            check();
+        } else if (quastion < answer) {
+            alert ("Загаданное число больше");
+            check();
+        } else if (quastion <= 100 && quastion >= 0) {
+            return confirm("Вы угадали!");
+        };
+    }
+    check();
 }
-check();
+game(55);
